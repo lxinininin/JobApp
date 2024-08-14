727a646e791b53efa6799ac2a8874df751e43ca7
linxin <lxinin1999@gmail.com>
2024-08-14 14:41:47 +0800

    Connecting React and Spring

--- src/main/java/com/linxin/SpringBootRest/JobRestController.java
+++ src/main/java/com/linxin/SpringBootRest/JobRestController.java
@@ -5,6 +5,7 @@ import com.linxin.SpringBootRest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController // @Controller + @ResponseBody
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 
     @Autowired
