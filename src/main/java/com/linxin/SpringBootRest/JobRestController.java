247a6dc97abee91465cc6bc8670f4ed3ce7f8f48
linxin <lxinin1999@gmail.com>
2024-08-14 15:45:38 +0800

    Put and Delete Mapping

--- src/main/java/com/linxin/SpringBootRest/JobRestController.java
+++ src/main/java/com/linxin/SpringBootRest/JobRestController.java
@@ -33,4 +33,16 @@ public class JobRestController {
         return service.getJob(jobPost.getPostId());
     }
 
+    @PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost) {
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable("postId") int postId) {
+        service.deleteJob(postId);
+        return "deleted";
+    }
+
 }
