7707758a4696880c27f15d71dc071064677eb595
linxin <lxinin1999@gmail.com>
2024-08-15 12:35:23 +0800

    AOP: After Advice

--- src/main/java/com/linxin/SpringBootRest/aop/LoggingAspect.java
+++ src/main/java/com/linxin/SpringBootRest/aop/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.linxin.SpringBootRest.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,6 +18,26 @@ public class LoggingAspect {
             "|| execution(* com.linxin.SpringBootRest.service.JobService.updateJob(..))")
     public void logMethodCall(JoinPoint jp) {
         LOGGER.info(jp.getSignature().getName() + " method called");
+    }
+
+    @After("execution(* com.linxin.SpringBootRest.service.JobService.getJob(..)) " +
+            "|| execution(* com.linxin.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodExecuted(JoinPoint jp) {
+        LOGGER.info(jp.getSignature().getName() + " method executed");
+    }
+
+    // call when has some errors
+    @AfterThrowing("execution(* com.linxin.SpringBootRest.service.JobService.getJob(..)) " +
+            "|| execution(* com.linxin.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodCrash(JoinPoint jp) {
+        LOGGER.info(jp.getSignature().getName() + " method has some issues");
+    }
+
+    // call when has no errors
+    @AfterReturning("execution(* com.linxin.SpringBootRest.service.JobService.getJob(..)) " +
+            "|| execution(* com.linxin.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint jp) {
+        LOGGER.info(jp.getSignature().getName() + " method executed successfully");
     }
 
 }
