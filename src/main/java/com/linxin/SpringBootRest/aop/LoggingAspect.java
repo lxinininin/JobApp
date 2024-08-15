530c156762a1fb94760677ac7cc8d06dd3c3d941
linxin <lxinin1999@gmail.com>
2024-08-15 11:56:06 +0800

    AOP: Before Advice

--- src/main/java/com/linxin/SpringBootRest/aop/LoggingAspect.java
+++ src/main/java/com/linxin/SpringBootRest/aop/LoggingAspect.java
@@ -1,12 +1,20 @@
 package com.linxin.SpringBootRest.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
+    // we have to mention return type, class-name.method name(args)
+    // @Before("execution(* *.*(..))") // all return types, all classes, all methods and any arguments
+    @Before("execution(* com.linxin.SpringBootRest.service.JobService.*(..))")
     public void logMethodCall() {
         LOGGER.info("Method called");
     }
